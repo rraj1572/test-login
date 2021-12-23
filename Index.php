44faef581b0e12d5a7d8b2e266d927e4ea6985f1
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2021-12-24 00:23:20 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,21 +1,60 @@
-<?php 
-// Start session
-session_start(); 
+Elijah: You want all users to use the same username/password? – 
+Sampson
+ Sep 4 '09 at 20:02
+1
+No, not quite really, but now i saw that Mark posted a quick and dirty solution and i use it now! Thanks everybody for your help, i appreciate your time. – 
+Elijah
+ Sep 4 '09 at 20:04
+2
+@Elijah: The solution Mark provided is fine if you only one small list of users. That solution won't scale very well. – 
+Sampson
+ Sep 4 '09 at 20:05
+Show 2 more comments
+10 Answers
+order by 
+votes
+Up vote
+24
+Down vote
+Accepted
+It's not an ideal solution but here's a quick and dirty example that shows how you could store login info in the PHP code:
 
-// Username and password
-$ID = "admin";
-$pass = "123456";
+<?php
+session_start();
 
-if (isset($_POST["ID"]) && isset($_POST["pass"])) { 
+$userinfo = array(
+                'user1'=>'password1',
+                'user2'=>'password2'
+                );
 
-    if ($_POST["ID"] === $anvandarID && $_POST["pass"] === $pass) { 
-    /
-    $_SESSION["inloggedin"] = true; 
-
-    header("Location: safe_site.php"); 
-    exit; 
-    } 
-        // Wrong login - message
-        else {$wrong = "Bad ID and password, the system could not log you in";} 
+if(isset($_GET['logout'])) {
+    $_SESSION['username'] = '';
+    header('Location:  ' . $_SERVER['PHP_SELF']);
 }
-?> 
+
+if(isset($_POST['username'])) {
+    if($userinfo[$_POST['username']] == $_POST['password']) {
+        $_SESSION['username'] = $_POST['username'];
+    }else {
+        //Invalid Login
+    }
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <?php if($_SESSION['username']): ?>
+            <p>You are logged in as <?=$_SESSION['username']?></p>
+            <p><a href="?logout=1">Logout</a></p>
+        <?php endif; ?>
+        <form name="login" action="" method="post">
+            Username:  <input type="text" name="username" value="" /><br />
+            Password:  <input type="password" name="password" value="" /><br />
+            <input type="submit" name="submit" value="Submit" />
+        </form>
+    </body>
+</html>
