5a7bc9feae63be42b3c7dcf683c155385b49ac9b
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2021-12-24 00:11:47 +0530

    Update and rename 1a-login.php to Index.php

---
+++ Index.php
@@ -0,0 +1,21 @@
+<?php 
+// Start session
+session_start(); 
+
+// Username and password
+$ID = "admin";
+$pass = "123456";
+
+if (isset($_POST["ID"]) && isset($_POST["pass"])) { 
+
+    if ($_POST["ID"] === $anvandarID && $_POST["pass"] === $pass) { 
+    /
+    $_SESSION["inloggedin"] = true; 
+
+    header("Location: safe_site.php"); 
+    exit; 
+    } 
+        // Wrong login - message
+        else {$wrong = "Bad ID and password, the system could not log you in";} 
+}
+?> 
