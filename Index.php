93a439ea8174ca7ac1223b444bc88e5c2dadd35f
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2021-12-24 00:23:53 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,24 +1,4 @@
-Elijah: You want all users to use the same username/password? – 
-Sampson
- Sep 4 '09 at 20:02
-1
-No, not quite really, but now i saw that Mark posted a quick and dirty solution and i use it now! Thanks everybody for your help, i appreciate your time. – 
-Elijah
- Sep 4 '09 at 20:04
-2
-@Elijah: The solution Mark provided is fine if you only one small list of users. That solution won't scale very well. – 
-Sampson
- Sep 4 '09 at 20:05
-Show 2 more comments
-10 Answers
-order by 
-votes
-Up vote
-24
-Down vote
-Accepted
-It's not an ideal solution but here's a quick and dirty example that shows how you could store login info in the PHP code:
-
+L
 <?php
 session_start();
 
